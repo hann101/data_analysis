d74d92835196546e9932ad93d3b7f82f1fc96ed9
Kenneth, Lee <edowon0623@gmail.com>
2021-03-05 14:59:13 +0900

    upload matplotib samples

---
+++ matplotlib/0.simple_matplotlib.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+
+plt.plot(["Seoul", "Tokyo", "New York"], [19, 7, 25])
+plt.xlabel("City")
+plt.ylabel("Value")
+plt.title("Sample #1")
+
+plt.show()
--- matplotlib/1.matplotlib_basic_bar.py
+++ matplotlib/1.matplotlib_basic_bar.py
@@ -7,7 +7,7 @@ customers_index = range(len(customers))
 sale_amounts = [127, 90, 201, 111, 232]
 
 fig = plt.figure()
-ax1 = fig.add_subplot(1,1,1)
+ax1 = fig.add_subplot(1,2,1)
 ax1.bar(customers_index, sale_amounts, align='center', color='darkblue')
 ax1.xaxis.set_ticks_position('bottom')
 ax1.yaxis.set_ticks_position('left')
